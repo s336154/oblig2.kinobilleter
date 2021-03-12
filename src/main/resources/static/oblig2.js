7905334dd6dd3ce52b8436ff3ef6b0abc39a7817
s336154 <70687346+s336154@users.noreply.github.com>
2021-03-12 09:27:30 +0100

    almost done with the project again

--- src/main/resources/static/oblig2.js
+++ src/main/resources/static/oblig2.js
@@ -59,24 +59,60 @@ $(() => {
         }
 
         const hent = () => $.get("/kino", filmer => formater(filmer));
-        const inputval = film => {
-            if (film.antall === "") return false
-            else if (film.fornavn === "") return false
-            else if (film.etternavn === "") return false
-            else if (film.telefonnr === "") return false
-            else if (film.epost === "") return false
-            else if (film.velgfilm === "") return false
-        }
-        const formater = filmer => {
-            let ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th>" +
-                "<th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
-            for (let film of filmer) {
-                ut += "<tr><td>" + film.velgfilm + "</td><td>" + film.antall + "</td><td>" + film.fornavn + "</td>" +
-                    "<td>" + film.etternavn + "</td><td>" + film.telefonnr + "</td><td>" + film.epost + "</td></tr>";
+        function check () {
+            let ErrorX = false;
+
+            let filmF = velgfilm;
+            let antallF = antall;
+            let fornavnF = fornavn;
+            let etternavnF = etternavn;
+            let telefonnrF = telefonnr;
+            let epostF = epost;
+
+
+            if (filmF === "velg film her") {
+                $("#filmX").html("må velge film");
+                ErrorX = true;
+            } else if (antallF === "") {
+                $("#antallX").html("må skrive noe inn i antall");
+                ErrorX = true;
+            } else if (antallF === "NaN") {
+                $("#antallX").html("må skrive tall i antall");
+                ErrorX = true;
+            } else if (fornavnF === "") {
+                $("fornavnX").html("må skrive noe inn i fornavnet");
+                ErrorX = true;
+            } else if (etternavnF === "") {
+                $("etternavnX").html("må skrive noe inn etternavnet");
+                ErrorX = true;
+            } else if (telefonnrF === "") {
+                $("telefonnrX").html("må skrive noe inn i telefonnr");
+                ErrorX = true;
+            } else if (telefonnrF === "NaN") {
+                $("telefonnrX").html("må skrive noe inn riktig telefonnr");
+                ErrorX = true;
+            } else if (epostF === "") {
+                $("epostX").html("må skrive noe inn i epost");
+                ErrorX = true;
+            } if (ErrorX === true) {
+                    return;
+
+                } else {
+                    const formater = filmer => {
+                        let ut = "<table>" +
+                            "<tr><th>Film</th><th>Antall</th><th>Fornavn</th>" +
+                            "<th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
+                        for (let film of filmer) {
+                            ut += "<tr><td>" + film.velgfilm + "</td><td>" + film.antall + "</td><td>" + film.fornavn + "</td>" +
+                                "<td>" + film.etternavn + "</td><td>" + film.telefonnr + "</td><td>" + film.epost + "</td></tr>";
+                        }
+                        ut += "</table>";
+                        $("#filmene").html(ut);
+                    }
+                }
             }
-            ut += "</table>";
-            $("#filmene").html(ut);
-        }
-    });
-});
+            });
+        });
+
+
 
