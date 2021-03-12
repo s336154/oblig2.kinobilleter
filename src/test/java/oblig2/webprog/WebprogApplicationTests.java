a180d43316ac7010dc73051895a972a5199b041f
s336154 <70687346+s336154@users.noreply.github.com>
2021-03-12 10:30:36 +0100

    springboot config

--- src/test/java/oblig2/webprog/WebprogApplicationTests.java
+++ src/test/java/oblig2/webprog/WebprogApplicationTests.java
@@ -1,9 +1,11 @@
 package oblig2.webprog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class WebprogApplicationTests {
 
     @Test
