60043632ef062d8a7042b1d5858a230d8ceefc64
s336154 <70687346+s336154@users.noreply.github.com>
2021-03-12 15:08:03 +0100

    changes

--- src/main/java/oblig2/webprog/KinoController.java
+++ src/main/java/oblig2/webprog/KinoController.java
@@ -5,28 +5,27 @@ import org.springframework.web.bind.annotation.*;
 
 
 import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/lagringserver/")
+
 public class KinoController {
 
 
-    @Autowired
-    AppRepository sal;
-
+    private final List<Billet> billeter= new ArrayList<>();
 
     @PostMapping("/kino")
-    public void lagre(Billet enBillet) {
-        sal.leggInn(enBillet);
+    public void lagreBillet(Billet enBillet){
+        billeter.add(enBillet);
     }
     @GetMapping("/kino")
-    public ArrayList<Billet> hent() {
-        return sal.hentAlle();
+    public List<Billet> hentAlle(){
+        return billeter;
     }
 
-
-    @DeleteMapping("/kino")
-    public void slett() {
-        sal.slettAlle();
+    @GetMapping("/kino")
+    public void slettAlle(){
+        billeter.clear();
     }
 
 
--- src/main/resources/static/oblig2.js
+++ src/main/resources/static/oblig2.js
@@ -1,43 +1,40 @@
 $(() => {
     $("#regFilm").click(() => {
 
-        const velgfilm = $("#velgfilm");
-        const antall = $("#antall");
-        const fornavn = $("#navn");
-        const etternavn = $("#etternavn");
-        const telefonnr = $("#telefonnr");
-        const epost = $("#epost");
-        const film = {
-            velgfilm: velgfilm.val(),
-            antall: antall.val(),
-            fornavn: fornavn.val(),
-            etternavn: etternavn.val(),
-            telefonnr: telefonnr.val(),
-            epost: epost.val(),
+        const velgfilm = $("#velgfilm").val();
+        const antall = $("#antall").val();
+        const fornavn = $("#navn").val();
+        const etternavn = $("#etternavn").val();
+        const telefonnr = $("#telefonnr").val();
+        const epost = $("#epost").val();
+
+        const enBillet = {
+            velgfilm: velgfilm,
+            antall: antall,
+            fornavn: fornavn,
+            etternavn: etternavn,
+            telefonnr: telefonnr,
+            epost: epost,
         };
+ 
 
-            if (inputval(enBillet)) {
-                $.post("/kino", enBillet, () => hent());
-                antall.val("");
-                fornavn.val("");
-                etternavn.val("");
-                telefonnr.val("");
-                epost.val("");
-            } else {
-                console.log("Mangler input");
-            }
-
-            $("#slettAlle").click(() => {
-                $.ajax("/kino", {
-                    type: 'DELETE',
-                    success: () => hent(),
-                    error: (jqXhr, textStatus, errorMessage) => console.log(errorMessage)
-                });
+        $.post("/kino", enBillet, function(){
+            hentAlle();
+        });
+        function hentAlle() {
+            $.get( "/kino", function( data ) {
+                formaterData(data);
             });
+        }
 
 
-        const hent = () => $.get("/kino", filmer => formater(filmer));
-        function check () {
+        /*   $.post("/kino", enBillet, function (){
+               $.get("/kino", function (billeter) {
+                   formater(billeter);
+               });
+           }); */
+
+
             let ErrorX = false;
 
             let filmF = velgfilm;
@@ -58,41 +55,45 @@ $(() => {
                 $("#antallX").html("må skrive tall i antall");
                 ErrorX = true;
             } else if (fornavnF === "") {
-                $("fornavnX").html("må skrive noe inn i fornavnet");
+                $("#fornavnX").html("må skrive noe inn i fornavnet");
                 ErrorX = true;
             } else if (etternavnF === "") {
-                $("etternavnX").html("må skrive noe inn etternavnet");
+                $("#etternavnX").html("må skrive noe inn etternavnet");
                 ErrorX = true;
             } else if (telefonnrF === "") {
-                $("telefonnrX").html("må skrive noe inn i telefonnr");
+                $("#telefonnrX").html("må skrive noe inn i telefonnr");
                 ErrorX = true;
             } else if (telefonnrF === "NaN") {
-                $("telefonnrX").html("må skrive noe inn riktig telefonnr");
+                $("#telefonnrX").html("må skrive noe inn riktig telefonnr");
                 ErrorX = true;
             } else if (epostF === "") {
-                $("epostX").html("må skrive noe inn i epost");
+                $("#epostX").html("må skrive noe inn i epost");
                 ErrorX = true;
             }
             if (ErrorX === true) {
                 return;
+            }
 
-            } else {
-                const formater = filmer => {
+            else {
+                function formaterData(billeter){
                     let ut = "<table>" +
                         "<tr><th>Film</th><th>Antall</th><th>Fornavn</th>" +
                         "<th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
-                    for (let film of filmer) {
+                    for (let film of billeter) {
                         ut += "<tr><td>" + film.velgfilm + "</td><td>" + film.antall + "</td><td>" + film.fornavn + "</td>" +
                             "<td>" + film.etternavn + "</td><td>" + film.telefonnr + "</td><td>" + film.epost + "</td></tr>";
                     }
                     ut += "</table>";
                     $("#filmene").html(ut);
-                }
-            }
-        }
+                } }
+
+        function slettAlle() {
+            $.get("/kino", enBillet, function () {
 
             });
-        });
+        }
 
+    });
+});
 
 
